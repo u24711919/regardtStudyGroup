e144741eefe2dafda3140295dfbaf6fdca3cebe5
Regardt van Heerden <u24711919@up.ac.za>
2025-03-03 11:59:47 +0200

    Add project files.

---
+++ regardtStudyGroup/Controllers/StudyGroupController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace regardtStudyGroup.Controllers
+{
+    public class StudyGroupController : Controller
+    {
+        // GET: StudyGroup
+        public ActionResult StudyGroupView()
+        {
+            List<Models.StudyGroupDetails> Members = new List<Models.StudyGroupDetails>();
+
+            Members.Add(new Models.StudyGroupDetails { StudentNumber = "u24711919", Firstname = "Regardt", Surname = "van Heerden", EmailAddress = "u24711919@tuks.co.za" });
+            Members.Add(new Models.StudyGroupDetails { StudentNumber = "u24664929", Firstname = "Arno", Surname = "Smalberger", EmailAddress = "u24664929@tuks.co.za" });
+            Members.Add(new Models.StudyGroupDetails { StudentNumber = "u24592961", Firstname = "Le-Andri", Surname = "Viviers", EmailAddress = "u24592961@tuks.co.za" });
+            Members.Add(new Models.StudyGroupDetails { StudentNumber = "u04581297", Firstname = "Tian", Surname = "Stoltz", EmailAddress = "u04581297@tuks.co.za" });
+            Members.Add(new Models.StudyGroupDetails { StudentNumber = "u24612066", Firstname = "Eghardt", Surname = "du Tiot", EmailAddress = "u24612066@tuks.co.za" });
+            return View(Members);
+        }
+    }
+}
---
+++ regardtStudyGroup/Models/StudyGroupDetails.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace regardtStudyGroup.Models
+{
+    public class StudyGroupDetails
+    {
+        [Display(Name = "Student Number")]
+        public string StudentNumber { get; set; }
+
+        [Display(Name = "Name")]
+        public string Firstname { get; set; }
+
+        [Display(Name = "Surname")]
+        public string Surname { get; set; }
+
+        [Display(Name = "Email Address")]
+        public string EmailAddress { get; set; }
+    }
+}
